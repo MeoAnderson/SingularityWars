ffda42a114cfa08efc72baa1a6cc12319d44f5e8
Vasco Patrício <meoanderson4@gmail.com>
2014-03-02 02:18:23 +0000

    Added particles, fade-in animations & other goodies

--- Projectfiles/SingularityWarsMenu.m
+++ Projectfiles/SingularityWarsMenu.m
@@ -29,7 +29,7 @@
 	if ((self = [super init]))
 	{
         // Background music
-		[[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"level.mp3"];//play background music
+		[[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"menu.mp3"];
 
 		
         // Sprite with background, and its position (at the center of the screen)
@@ -37,35 +37,89 @@
         CCSprite* background = [CCSprite spriteWithFile:@"Background.png"];
         background.anchorPoint = ccp(0,0);
         [self addChild:background];
-        
-        // Grid (same size as the background), with a ripple effect
-       
-        CCRipple3D* backgroundWaves = [CCRipple3D actionWithDuration:10 size:ccg(0.1,0.1) position:ccp(0,0) radius:50.0f waves:5 amplitude:55.0f];
+		
+		// Fade-in animation for background
+		
+		CCFadeIn* animatingBackground = [CCFadeIn actionWithDuration:3];
+		animatingBackground.duration = 3;
+		[background runAction:animatingBackground];
+		
+		//Background particles
+		
+		CCParticleExplosion* particulasBackground = [CCParticleExplosion particleWithTotalParticles:2000];
+		particulasBackground.speed = 150.0f;
+		particulasBackground.duration = -1;
+		particulasBackground.emissionRate = 700;
+		particulasBackground.sourcePosition = ccp(0,0);
+		[self addChild:particulasBackground];
+		
+//		CCSprite* particleTexture = [CCSprite spriteWithFile:@"particle.png"];
+//		[particulasBackground addChild:particleTexture];
 
-        // Repeat the "backgroundWaves" instance forever
+			CCWaves3D* backgroundWaves = [CCWaves3D actionWithDuration:40
+															size:ccg(20,20)
+														   waves:10
+													   amplitude:10.0f];
+		
+        // Repeat the "backgroundWaves" instance forever, and apply it to the background sprite
         CCRepeatForever* repeatWaves = [CCRepeatForever actionWithAction:backgroundWaves];;
         [background runAction:repeatWaves];
         
-        //[background runAction: [CCRepeatForever actionWithAction: backgroundWaves]];
-        
         
 		// "Singularity Wars" title, with its attributes
-		CCLabelTTF* title = [CCLabelTTF labelWithString:@"Singularity Wars" fontName:@"technoid" fontSize:70];
-		title.position = [CCDirector sharedDirector].screenCenter;
-		//title.position = ccp(0.0f, 0.0f);
+		CCLabelTTF* title = [CCLabelTTF labelWithString:@"Singularity Wars" fontName:@"technoid" fontSize:80];
+		title.anchorPoint = ccp(0,0);
+		title.position = ccp(70,650);
 		title.color = ccWHITE;
 		[self addChild:title];
 		
+		// Fade-in animation for title
+		
+		CCFadeIn* animatingTitle = [CCFadeIn actionWithDuration:3];
+		animatingTitle.duration = 3;
+		[title runAction:animatingTitle];
+	
+		
 		// "Singularity Wars" subtitle, "ABGWR" 
-		CCLabelTTF* subTitle = [CCLabelTTF labelWithString:@"-ABGWR-" fontName:@"technoid" fontSize:30];
-		subTitle.position = [CCDirector sharedDirector].screenCenter;
+		CCLabelTTF* subTitle = [CCLabelTTF labelWithString:@"-ABGWR-" fontName:@"technoid" fontSize:45];
+		subTitle.anchorPoint = ccp(0,0);
+		subTitle.position = ccp(400,600);
 		subTitle.color = ccWHITE;
 		[self addChild:subTitle];
 		
-        
+		// Fade-in animation for subtitle
+		
+		CCFadeIn* animatingSubTitle = [CCFadeIn actionWithDuration:3];
+		animatingSubTitle.duration = 3;
+		[subTitle runAction:animatingSubTitle];
+		
+
+
+       	
 	}
 
 	return self;
 }
 
 @end
+
+
+/*
+ 
+ UNUSED CODE
+ 
+ // Grid sprite
+ CCSprite* grid = [CCSprite spriteWithFile:@"grid.png"];
+ grid.anchorPoint = ccp(0,0);
+ [self addChild:grid];
+ */
+// Grid (same size as the background), with a ripple effect
+/*
+ CCRipple3D* backgroundWaves = [CCRipple3D actionWithDuration:10
+ size:ccg(20,20)
+ position:ccp(20,20)
+ radius:1.0f
+ a	   waves:3
+ amplitude:20.0f];
+ 
+ */
