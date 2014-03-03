52e7bfb8723573f8f200ae9d8f4ae24f6fdfbd7c
Vasco Patrício <meoanderson4@gmail.com>
2014-03-03 23:39:21 +0000

    Preparation for bigger changes

--- Projectfiles/Menu.m
+++ Projectfiles/Menu.m
@@ -1,9 +1,9 @@
 /*
  * Kobold2D™ --- http://www.kobold2d.org
  *
- * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Copyright (c) 2010-2011 Steffen Itterheim.
  * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
---------------------------------------------------------------------------------------------------
+ --------------------------------------------------------------------------------------------------
  Master in Computer Engineering
  
  Mobile Applications Development
@@ -30,22 +30,27 @@
 	{
         // Background music
 		[[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"menu2.m4a"];
-
+		
 		
         // Sprite with background, and its position (at the center of the screen)
-        
-        CCSprite* background = [CCSprite spriteWithFile:@"Background2.png"];
+        CCSprite* background = [CCSprite spriteWithFile:@"teste.png"];
         background.anchorPoint = ccp(0,0);
         [self addChild:background];
 		
-		// Fade-in animation for background
+		/*
+		// Grid, above background
+        CCSprite* grid = [CCSprite spriteWithFile:@"grid.png"];
+        grid.anchorPoint = ccp(0,0);
+        [self addChild:grid];
+		*/
 		
+		// Fade-in animation for background
 		CCFadeIn* animatingBackground = [CCFadeIn actionWithDuration:2];
 		animatingBackground.duration = 2;
 		[background runAction:animatingBackground];
 		
-		//Background particles
 		
+		//Background particles
 		CCParticleExplosion* particulasBackground = [CCParticleExplosion particleWithTotalParticles:2000];
 		particulasBackground.speed = 150.0f;
 		particulasBackground.duration = -1;
@@ -54,14 +59,12 @@
 		particulasBackground.startColor = ccc4f(0.1f, 0.7f, 0.3f, 1.0f);
 		[self addChild:particulasBackground];
 		
-//		CCSprite* particleTexture = [CCSprite spriteWithFile:@"particle.png"];
-//		[particulasBackground addChild:particleTexture];
-
-			CCWaves3D* backgroundWaves = [CCWaves3D actionWithDuration:40
-															size:ccg(20,20)
-														   waves:10
-													   amplitude:10.0f];
 		
+		CCWaves3D* backgroundWaves = [CCWaves3D actionWithDuration:40
+															  size:ccg(20,20)
+															 waves:15
+														 amplitude:20.0f];
+
         // Repeat the "backgroundWaves" instance forever, and apply it to the background sprite
         CCRepeatForever* repeatWaves = [CCRepeatForever actionWithAction:backgroundWaves];;
         [background runAction:repeatWaves];
@@ -74,31 +77,28 @@
 		title.color = ccWHITE;
 		[self addChild:title];
 		
-		// Fade-in animation for title
 		
+		// Fade-in animation for title
 		CCFadeIn* animatingTitle = [CCFadeIn actionWithDuration:3];
 		animatingTitle.duration = 3;
 		[title runAction:animatingTitle];
-	
 		
-		// "Singularity Wars" subtitle, "ABGWR" 
+		
+		// "Singularity Wars" subtitle, "ABGWR"
 		CCLabelTTF* subTitle = [CCLabelTTF labelWithString:@"-ABGWR-" fontName:@"technoid" fontSize:45];
 		subTitle.anchorPoint = ccp(0,0);
 		subTitle.position = ccp(400,600);
 		subTitle.color = ccWHITE;
 		[self addChild:subTitle];
 		
-		// Fade-in animation for subtitle
 		
+		// Fade-in animation for subtitle
 		CCFadeIn* animatingSubTitle = [CCFadeIn actionWithDuration:3];
 		animatingSubTitle.duration = 3;
 		[subTitle runAction:animatingSubTitle];
 		
-
-
-       	
 	}
-
+	
 	return self;
 }
 
